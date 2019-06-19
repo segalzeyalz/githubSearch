cb352bf906fa1228d4eeaedaf48a4214cde08ee6
segalzeyalz <segaleseo@gmail.com>
2019-06-19 22:57:22 +0300

    adding search

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import InputBase from '@material-ui/core/InputBase';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { fade } from '@material-ui/core/styles/colorManipulator';
+import Fab from '@material-ui/core/Fab';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -22,12 +21,8 @@ const styles = theme => ({
         '&:hover': {
             backgroundColor: fade(theme.palette.common.white, 0.25),
         },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing.unit,
-            width: 'auto',
-        },
+        marginLeft: theme.spacing(2),
+        width: '75%'
     },
     searchIcon: {
         width: theme.spacing(7),
@@ -45,10 +40,10 @@ const styles = theme => ({
     inputInput: {
         padding: theme.spacing(1, 1, 1, 7),
         transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        }
+        width: '100%'
+    },
+    fab: {
+        margin: theme.spacing(1),
     }
 });
 const GithubSearcher = ({
@@ -63,16 +58,8 @@ const GithubSearcher = ({
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton
-                    edge="start"
-                    className={classes.menuButton}
-                    color="inherit"
-                    aria-label="Open drawer"
-                >
-                    <MenuIcon />
-                </IconButton>
                 <Typography className={classes.title} variant="h6" noWrap>
-                    Material-UI
+                    Github Users Search
                 </Typography>
                 <div className={classes.search}>
                     <div className={classes.searchIcon}>
@@ -88,6 +75,9 @@ const GithubSearcher = ({
                         inputProps={{ 'aria-label': 'Search' }}
                     />
                 </div>
+                <Fab color="secondary" aria-label="Search" className={classes.fab} onClick={handleClick}>
+                    <SearchIcon />
+                </Fab>
             </Toolbar>
         </AppBar>
     );
