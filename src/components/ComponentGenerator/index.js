e80f0e23d9fa42d060806ed0711cb030c80a6c07
segalzeyalz <segaleseo@gmail.com>
2019-06-23 22:54:59 +0300

    add comment

--- src/components/ComponentGenerator/index.js
+++ src/components/ComponentGenerator/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import {TEXT_TYPE, IMAGE_TYPE} from './types';
 
+//This function return an component by the type of the field
 const ComponentByType = (type, value) => {
     if(type===TEXT_TYPE){
         return (<Typography>{value}</Typography>)
     } else if(type===IMAGE_TYPE){
-        return <img alt="user avatar" src={value} />
+        return <img height="100px" alt="user avatar" src={value} />
     } else{
         return (<Typography>{value}</Typography>)
     }
